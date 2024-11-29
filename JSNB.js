613dce9e85e1055b689d8744d1edb8b8b4820fbc
BlazeX <oleksanderbas704@gmail.com>
2024-11-29 19:19:40 +0200

    Update JSNB.js

--- JSNB.js
+++ JSNB.js
@@ -13,9 +13,9 @@ languageToggle.addEventListener('click', () => {
                 JSNB — це не просто компанія. Це сила, що діє в тіні, тихо змінюючи світ. 
                 Відомі небагатьом, але обговорювані багатьма, ми впливаємо на більше, ніж видно.
             </p>
-            <h2>Чому RIZIGOOD?</h2>
+            <h2>Чому BlazeX?</h2>
             <p>
-                RIZIGOOD приєднався до нашої команди! Його подорож разом із JSNB лише починається, і ми впевнені, що попереду багато цікавого.
+               BlazeX приєднався до нашої команди! Його подорож разом із JSNB лише починається, і ми впевнені, що попереду багато цікавого.
             </p>
             <h2>Що ми робимо?</h2>
             <p>
@@ -33,9 +33,9 @@ languageToggle.addEventListener('click', () => {
                 JSNB is not just a company. It’s a force hidden in the shadows, silently reshaping the world. 
                 Known by few, discussed by many, our influence extends beyond the visible.
             </p>
-            <h2>Why RIZIGOOD?</h2>
+            <h2>Why BlazeX?</h2>
             <p>
-                RIZIGOOD has joined our team! His journey with JSNB is just beginning, and we're excited for what lies ahead.
+                BlazeX has joined our team! His journey with JSNB is just beginning, and we're excited for what lies ahead.
             </p>
             <h2>What We Do?</h2>
             <p>
